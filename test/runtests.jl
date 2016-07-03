0d14c7ae7fa93e29a5ae17c33747631e68efe3be
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-07-03 18:43:47 +0100

    vaguely better test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,15 @@ using Spglib
 using FactCheck
 
 facts("Get Symmetry of simple lattices") do
-  facts("fcc") do
-    symops = symmetry_operations(
-        [0 0.5 0.5; 0.5 0 0.5; 0.5 0.5 0], transpose([0 0 0]), [1])
+  facts("diamond") do
+    cell = [0 0.5 0.5; 0.5 0 0.5; 0.5 0.5 0]
+    positions = transpose([0 0 0; 0.25 0.25 0.25])
+
+    symops = symmetry_operations(cell, positions, [:Si, :Si])
     @fact length(symops) --> 48
+
+    symops = symmetry_operations(cell, positions, [:Zn, :S])
+    @fact length(symops) --> 24
   end
 end
 exitstatus()
