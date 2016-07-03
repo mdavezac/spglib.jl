3590d9df1432d86422c1383aa05f41b2020802c2
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-07-03 11:26:07 +0100

    Download and build spglib

---
+++ deps/build.jl
@@ -0,0 +1,9 @@
+using BinDeps
+
+@BinDeps.setup
+
+libspg = library_dependency("libsymspg")
+provides(Sources, URI("https://sourceforge.net/projects/spglib/files/spglib/spglib-1.9/spglib-1.9.4.tar.gz"), libspg)
+provides(BuildProcess, Autotools(libtarget = joinpath("src", "libsymspg.la")), libspg)
+
+@BinDeps.install Dict(:spglib => :libspg)
